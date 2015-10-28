9c8fa2e11f65ce354a29c1595d749bbad6f3ea01
Sebastien Cagnon <cagnonsebastien@gmail.com>
2015-10-28 16:04:08 +0900

    feat: added call to MovieDB api and logged output (no JSON Parsing yet
    added FetchMovieInfo that extends AsyncTask
    added ignored file Constants.java to put my API key
    TODO: explain API use in README
    call the AsyncTask from the onStart method

--- app/src/main/java/com/example/android/bestmovies/MovieGridFragment.java
+++ app/src/main/java/com/example/android/bestmovies/MovieGridFragment.java
@@ -1,12 +1,21 @@
 package com.example.android.bestmovies;
 
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.GridView;
 
+import org.json.JSONException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -39,5 +48,85 @@ public class MovieGridFragment extends Fragment {
         movieGrid.setAdapter(movieAdapter);
 
         return rootView;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        updateMovieList();
+    }
+
+    /**
+     * calls the AsymcTask to fetch movies and populate the grid
+     */
+    public void updateMovieList() {
+        new FetchMovieInfo().execute();
+    }
+
+    private class FetchMovieInfo extends AsyncTask<Void, Void, MovieThumbnailFlavor[]> {
+
+        @Override
+        protected MovieThumbnailFlavor[] doInBackground(Void... params) {
+            Uri.Builder builder = new Uri.Builder();
+            builder.scheme("https")
+                    .authority("api.themoviedb.org")
+                    .appendPath("3")
+                    .appendPath("discover")
+                    .appendPath("movie")
+                    .appendQueryParameter("sort_by", "popularity.desc")
+                    .appendQueryParameter("api_key", Constants.MOVIEDB_API_KEY);
+            String targetUrl = builder.build().toString();
+
+            HttpURLConnection urlConnection = null;
+
+            String movieListJSON;
+            try {
+                URL url = new URL(targetUrl);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                InputStream inputStream = urlConnection.getInputStream();
+                if (inputStream==null) {
+                    return null;
+                }
+                movieListJSON = convertStreamToString(inputStream);
+                Log.d(LOG_CAT, movieListJSON);
+                try {
+                    return getMovieThumbnailListFromJSON(movieListJSON);
+                } catch (JSONException e) {
+                    Log.e(LOG_CAT, "JSON Conversion Error: " + e.toString(), e);
+                    return null;
+                }
+            } catch (IOException e) {
+                Log.e(LOG_CAT, "IOError: " + e.toString(), e);
+                return null;
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+            }
+        }
+
+        /**
+         * Simple InputStream to String found on StackOverflow
+         * @param is the input stream to convert
+         * @return the input stream accumulated into one string
+         */
+        private String convertStreamToString(InputStream is) {
+            java.util.Scanner s = new java.util.Scanner(is).useDelimiter("\\A");
+            return s.hasNext() ? s.next() : "";
+        }
+
+        /**
+         * Retrieves the data from the JSON and creates a list of objects containing movies info
+         * @param movieDataJSON the JSON received from MovieDB
+         * @return A list of movies formatted as nice MovieThumbnailFlavor
+         * @throws JSONException
+         */
+        private MovieThumbnailFlavor[] getMovieThumbnailListFromJSON(String movieDataJSON)
+            throws JSONException {
+            return null;
+        }
     }
 }
